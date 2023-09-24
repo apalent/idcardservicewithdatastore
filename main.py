80c2c40963da29e9d989f5cb3f997086e6ed58dd
apalent <145934550+apalent@users.noreply.github.com>
2023-09-24 17:43:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,100 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from fastapi import FastAPI, HTTPException, Path
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, String, Integer, update
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from databases import Database
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///id_card_database.db'
-db = SQLAlchemy(app)
+# Create a FastAPI instance
+app = FastAPI()
+Base = declarative_base()
+# Database setup
+DATABASE_URL = "sqlite:///./id_card.db"
+database = Database(DATABASE_URL)
 
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class IDCard(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    bank_name = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
-    date_of_birth = db.Column(db.String(20), nullable=False)
-    blood_group = db.Column(db.String(5), nullable=False)
+# Define the IDCard model
+class IDCard(Base):
+    __tablename__ = "id_cards"
 
-    def __init__(self, name, bank_name, phone_number, date_of_birth, blood_group):
-        self.name = name
-        self.bank_name = bank_name
-        self.phone_number = phone_number
-        self.date_of_birth = date_of_birth
-        self.blood_group = blood_group
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    bank_name = Column(String)
+    phone_number = Column(String, unique=True, index=True)
+    date_of_birth = Column(String)
+    blood_group = Column(String)
 
+# Pydantic model for ID card data
+class IDCardCreate(BaseModel):
+    name: str
+    bank_name: str
+    phone_number: str
+    date_of_birth: str
+    blood_group: str
 
-# Initialize the database within the application context
-with app.app_context():
-    db.create_all()
+class IDCardResponse(BaseModel):
+    id: int
+    name: str
+    bank_name: str
+    phone_number: str
+    date_of_birth: str
+    blood_group: str
 
+# Endpoint to save ID card details
+@app.post("/id_card/", response_model=IDCardResponse)
+async def create_id_card(id_card: IDCardCreate):
+    async with database.transaction():
+        # Check if the phone number already exists in the database
+        existing_record = await database.fetch_one(
+            "SELECT * FROM id_cards WHERE phone_number = :phone_number",
+            values={"phone_number": id_card.phone_number},
+        )
 
-@app.route('/id_card', methods=['POST'])
-def save_id_card():
-    data = request.json
-    name = data['name']
-    bank_name = data['bank_name']
-    phone_number = data['phone_number']
-    date_of_birth = data['date_of_birth']
-    blood_group = data['blood_group']
+        if existing_record:
+            # If the phone number exists, update the existing record
+            query = (
+                f"UPDATE id_cards SET "
+                f"name = :name, "
+                f"bank_name = :bank_name, "
+                f"date_of_birth = :date_of_birth, "
+                f"blood_group = :blood_group "
+                f"WHERE phone_number = :phone_number"
+            )
+            await database.execute(
+                query,
+                values=id_card.dict(),
+            )
+            id_card_record = await database.fetch_one(
+                "SELECT * FROM id_cards WHERE phone_number = :phone_number",
+                values={"phone_number": id_card.phone_number},
+            )
+        else:
+            # If the phone number doesn't exist, insert a new record
+            query = (
+                "INSERT INTO id_cards (name, bank_name, phone_number, date_of_birth, blood_group) "
+                "VALUES (:name, :bank_name, :phone_number, :date_of_birth, :blood_group)"
+            )
+            last_record_id = await database.execute(query, values=id_card.dict())
+            id_card_record = await database.fetch_one(
+                "SELECT * FROM id_cards WHERE id = :id",
+                values={"id": last_record_id},
+            )
 
-    id_card = IDCard(name=name, bank_name=bank_name, phone_number=phone_number,
-                     date_of_birth=date_of_birth, blood_group=blood_group)
+        return IDCardResponse(**id_card_record)
 
-    db.session.add(id_card)
-    db.session.commit()
+# Endpoint to get ID card details by phone number
+@app.get("/id_card/{phone_number}", response_model=IDCardResponse)
+async def read_id_card(phone_number: str = Path(..., title="The phone number of the ID card owner")):
+    id_card_record = await database.fetch_one(
+        "SELECT * FROM id_cards WHERE phone_number = :phone_number",
+        values={"phone_number": phone_number},
+    )
+    if id_card_record is None:
+        raise HTTPException(status_code=404, detail="ID card not found")
+    return IDCardResponse(**id_card_record)
 
-    return jsonify({"message": "ID card details saved successfully"}), 201
-
-
-@app.route('/id_card/<string:phone_number>', methods=['GET'])
-def get_id_card(phone_number):
-    id_card = IDCard.query.filter_by(phone_number=phone_number).first()
-    if id_card:
-        return jsonify({
-            "name": id_card.name,
-            "bank_name": id_card.bank_name,
-            "phone_number": id_card.phone_number,
-            "date_of_birth": id_card.date_of_birth,
-            "blood_group": id_card.blood_group
-        })
-    else:
-        return jsonify({"message": "ID card not found"}), 404
-
-
-@app.route('/id_card/<string:phone_number>', methods=['PUT'])
-def edit_id_card(phone_number):
-    id_card = IDCard.query.filter_by(phone_number=phone_number).first()
-    if id_card:
-        data = request.json
-        id_card.name = data['name']
-        id_card.bank_name = data['bank_name']
-        id_card.date_of_birth = data['date_of_birth']
-        id_card.blood_group = data['blood_group']
-
-        db.session.commit()
-        return jsonify({"message": "ID card details updated successfully"}), 200
-    else:
-        return jsonify({"message": "ID card not found"}), 404
-
-
-@app.route('/id_card/<string:phone_number>', methods=['DELETE'])
-def delete_id_card(phone_number):
-    id_card = IDCard.query.filter_by(phone_number=phone_number).first()
-    if id_card:
-        db.session.delete(id_card)
-        db.session.commit()
-        return jsonify({"message": "ID card deleted successfully"}), 200
-    else:
-        return jsonify({"message": "ID card not found"}), 404
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
