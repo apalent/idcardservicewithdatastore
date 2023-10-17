cb3b1aa0d10d3c8dbf882d0ddc5946cb5afcf9ae
apalent <145934550+apalent@users.noreply.github.com>
2023-10-16 21:12:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
+from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from databases import Database
@@ -21,7 +21,8 @@ class IDCard(Base):
     phone_number = Column(String, unique=True, index=True)
     date_of_birth = Column(String)
     blood_group = Column(String)
-    address = Column(String)  # Add the "address" field
+    address = Column(String)
+    branch = Column(String)  # Add the "branch" field as a string
 
 # Check if the database file exists, and create it if it doesn't
 if not os.path.exists("id_card.db"):
@@ -44,7 +45,8 @@ id_cards_table = Table(
     Column("phone_number", String, unique=True, index=True),
     Column("date_of_birth", String),
     Column("blood_group", String),
-    Column("address", String),  # Add the "address" field
+    Column("address", String),
+    Column("branch", String),  # Add the "branch" field
     extend_existing=True,
 )
 
@@ -59,7 +61,8 @@ class IDCardCreate(BaseModel):
     phone_number: str
     date_of_birth: str
     blood_group: str
-    address: str  # Add the "address" field
+    address: str
+    branch: str  # Add the "branch" field
 
 class IDCardResponse(BaseModel):
     id: int
@@ -68,7 +71,8 @@ class IDCardResponse(BaseModel):
     phone_number: str
     date_of_birth: str
     blood_group: str
-    address: str  # Add the "address" field
+    address: str
+    branch: str  # Add the "branch" field
 
 # Endpoint to save ID card details
 @app.post("/id_card/", response_model=IDCardResponse)
@@ -88,7 +92,8 @@ async def create_id_card(id_card: IDCardCreate):
                 f"bank_name = :bank_name, "
                 f"date_of_birth = :date_of_birth, "
                 f"blood_group = :blood_group, "
-                f"address = :address "  # Include the "address" field
+                f"address = :address, "  # Include the "address" field
+                f"branch = :branch "  # Include the "branch" field
                 f"WHERE phone_number = :phone_number"
             )
             await database.execute(
@@ -102,8 +107,8 @@ async def create_id_card(id_card: IDCardCreate):
         else:
             # If the phone number doesn't exist, insert a new record
             query = (
-                "INSERT INTO id_cards (name, bank_name, phone_number, date_of_birth, blood_group, address) "
-                "VALUES (:name, :bank_name, :phone_number, :date_of_birth, :blood_group, :address)"  # Include the "address" field
+                "INSERT INTO id_cards (name, bank_name, phone_number, date_of_birth, blood_group, address, branch) "
+                "VALUES (:name, :bank_name, :phone_number, :date_of_birth, :blood_group, :address, :branch)"  # Include the "address" and "branch" fields
             )
             last_record_id = await database.execute(query, values=id_card.dict())
             id_card_record = await database.fetch_one(
@@ -113,16 +118,43 @@ async def create_id_card(id_card: IDCardCreate):
 
         return IDCardResponse(**id_card_record)
 
-# Endpoint to get ID card details by phone number
-@app.get("/id_card/{phone_number}", response_model=IDCardResponse)
-async def read_id_card(phone_number: str):
-    id_card_record = await database.fetch_one(
-        "SELECT * FROM id_cards WHERE phone_number = :phone_number",
-        values={"phone_number": phone_number},
-    )
-    if id_card_record is None:
-        raise HTTPException(status_code=404, detail="ID card not found")
-    return IDCardResponse(**id_card_record)
+# Endpoint to get ID card details by phone number (optional)
+@app.get("/id_card/")
+async def read_id_card(phone_number: str = None):
+    if phone_number is not None:
+        # If a phone number is provided, return specific data
+        id_card_record = await database.fetch_one(
+            "SELECT * FROM id_cards WHERE phone_number = :phone_number",
+            values={"phone_number": phone_number},
+        )
+        if id_card_record is None:
+            raise HTTPException(status_code=404, detail="ID card not found")
+        return IDCardResponse(**id_card_record)
+    else:
+        # If no phone number is provided, return all data
+        id_card_records = await database.fetch_all("SELECT * FROM id_cards")
+        return id_card_records
+
+@app.delete("/id_card/{phone_number}", response_model=str)
+async def delete_id_card(phone_number: str):
+    async with database.transaction():
+        # Check if the phone number exists in the database
+        existing_record = await database.fetch_one(
+            "SELECT * FROM id_cards WHERE phone_number = :phone_number",
+            values={"phone_number": phone_number},
+        )
+
+        if not existing_record:
+            raise HTTPException(status_code=404, detail="ID card not found")
+
+        # If the phone number exists, delete the record
+        await database.execute(
+            "DELETE FROM id_cards WHERE phone_number = :phone_number",
+            values={"phone_number": phone_number},
+        )
+
+    return "ID card deleted successfully"
+
 
 # Run the FastAPI app
 if __name__ == "__main__":
